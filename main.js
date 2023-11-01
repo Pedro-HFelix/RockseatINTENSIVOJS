b601134dbb19b7d8c6470762665cd8afd74bef9c
Pedro-HFelix <108374688+Pedro-HFelix@users.noreply.github.com>
2023-10-31 22:14:31 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,66 @@
+// Exercício 1: Exibir "Hello World!" em um alerta
+alert("Hello World!");
+
+// Exercício 2: Declarar duas variáveis e exibir a soma entre elas
+var num1 = 5;
+var num2 = 7;
+var soma = num1 + num2;
+alert("A soma é: " + soma);
+
+// Exercício 3: Verificar se o valor é um número
+var valor1 = 42;
+if (typeof valor1 === "number") {
+  alert("É um número");
+} else {
+  alert("Não é um número");
+}
+
+// Exercício 4: Verificar se o valor é uma string
+var valor2 = "Olá, mundo!";
+if (typeof valor2 === "string") {
+  alert("É uma string");
+} else {
+  alert("Não é uma string");
+}
+
+// Exercício 5: Verificar se o valor é um booleano
+var valor3 = true;
+if (typeof valor3 === "boolean") {
+  alert("É um booleano");
+} else {
+  alert("Não é um booleano");
+}
+
+// Exercício 6: Declarar duas variáveis e exibir a subtração entre elas
+var num3 = 10;
+var num4 = 3;
+var subtracao = num3 - num4;
+alert("A subtração é: " + subtracao);
+
+// Exercício 7: Declarar duas variáveis e exibir a multiplicação entre elas
+var num5 = 4;
+var num6 = 6;
+var multiplicacao = num5 * num6;
+alert("A multiplicação é: " + multiplicacao);
+
+// Exercício 8: Declarar duas variáveis e exibir a divisão entre elas
+var num7 = 15;
+var num8 = 3;
+var divisao = num7 / num8;
+alert("A divisão é: " + divisao);
+
+// Exercício 9: Verificar se o valor é um número par
+var valor4 = 14;
+if (valor4 % 2 === 0) {
+  alert("É um número par");
+} else {
+  alert("Não é um número par");
+}
+
+// Exercício 10: Verificar se o valor é um número ímpar
+var valor5 = 7;
+if (valor5 % 2 !== 0) {
+  alert("É um número ímpar");
+} else {
+  alert("Não é um número ímpar");
+}
